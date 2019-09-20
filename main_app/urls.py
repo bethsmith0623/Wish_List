53e2f39c4ac8873b1f48e954628e217120b519c1
Beth Smith <bethsmith0623@gamil.com>
2019-09-20 15:00:41 -0500

    almost done

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,9 @@
+  
 from django.urls import path
 from . import views
 
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    
-
+urlpatterns= [
+    path('', views.index, name='index'),
+    path('add/',views.ItemCreate.as_view(), name='items_create'),
+    path('<int:pk>/delete/', views.ItemDelete.as_view(), name='items_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, redirect
+  
+from django.shortcuts import render
+from .models import Item
+from django.views.generic.edit import CreateView, DeleteView
+
+def index(request):
+    items = Item.objects.all()
+    return render(request, 'index.html',{'items': items})
 
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = '/'
